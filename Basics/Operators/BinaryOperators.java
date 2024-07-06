53a4de6defc4a43c221202dfa281125216f78e1e
Tarek-mahmoud <tarekashm@gmail.com>
2024-07-06 15:26:23 +0300

    Add files via upload

---
+++ Basics/Operators/BinaryOperators.java
@@ -0,0 +1,22 @@
+package Section5.Operators;
+
+// 5.1.2 Binary operators
+
+public class BinaryOperators {
+    public static void main(String[] args) {
+        /*
+            +
+            -
+            /
+            %
+            *
+         */
+
+        int num1 = 14;
+        int num2 = 3;
+        int result = num1 % num2;
+        System.out.println(result);
+
+        System.out.println("Tarek " + "Loves " + "Amina");
+    }
+}
